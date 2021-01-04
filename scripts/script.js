105821860af3f5811cc978a5c4cc68e6111fa0f2
christiaanbraun <74234093+christiaanbraun@users.noreply.github.com>
2021-01-04 10:15:20 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,36 @@
 // JavaScript Document
+var faders = document.querySelectorAll('.fade-in');
+var landenKnop = document.querySelector('.landenknop')
+var landenOverlay = document.querySelector('.landenoverlay')
+
+
+var appearOptions = {
+    treshold: 1,
+    rootMargin: "0px 0px -200px 0px",
+};
+
+var appearOnScroll = new IntersectionObserver
+( function(
+        entries,
+        appearOnScroll
+) {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            entry.target.classList.add('appear'); 
+            appearOnScroll.unobserve(entry.target);
+            }
+        });
+    },  
+    appearOptions);
+
+    faders.forEach(fader => { 
+        appearOnScroll.observe(fader);
+    })
+
+    landenKnop.addEventListener("click", openMenu);
+
+    function openMenu(){
+        landenOverlay.classList.toggle("visible");
+    }
