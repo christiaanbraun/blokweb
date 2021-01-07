26408b49d0f1c46f1cf39eb4b468517947decba1
christiaanbraun <74234093+christiaanbraun@users.noreply.github.com>
2021-01-07 16:51:54 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,34 @@
 // JavaScript Document
+// Menu fold open
+var menuButton = document.querySelector('header button');
+var body = document.querySelector('body');
+
+
+
+
+// de faders 
 var faders = document.querySelectorAll('.fade-in');
-var landenKnop = document.querySelector('.landenknop')
-var landenOverlay = document.querySelector('.landenoverlay')
+
+// de sliders
+var sliders = document.querySelectorAll('.slide-in');
+
+// Landenmenu footer
+var landenKnop = document.querySelector('.landenknop');
+var landenOverlay = document.querySelector('.landenoverlay');
 
 
-var appearOptions = {
+// Menu openen functie enzooooo
+menuButton.addEventListener("click", openNav);
+
+function openNav(){
+    body.classList.toggle('visible');
+
+
+}
+
+// Elementen in laten faden
+
+var appearOptionsFade = {
     treshold: 1,
     rootMargin: "0px 0px -200px 0px",
 };
@@ -23,14 +47,46 @@ var appearOnScroll = new IntersectionObserver
             }
         });
     },  
-    appearOptions);
+    appearOptionsFade);
 
     faders.forEach(fader => { 
         appearOnScroll.observe(fader);
     })
 
+// Elementen in laten sliden
+
+var appearOptionsSlide = {
+    treshold: 1,
+    rootMargin: "0px 0px -100px 0px",
+};
+
+var slideOnScroll =  new IntersectionObserver
+( function(
+    entries,
+    slideOnScroll    
+){ 
+    entries.forEach(entry => {
+        if (!entry.isIntersecting){
+            return;
+        }else {
+            entry.target.classList.add('slide-appear');
+            // entry.target.classList.remove('slide-in');
+            slideOnScroll.unobserve(entry.target);
+        }
+    });
+},
+appearOptionsSlide);
+
+sliders.forEach(slider => {
+    slideOnScroll.observe(slider);
+})
+
+
+    // Footer menu openen
+
     landenKnop.addEventListener("click", openMenu);
 
     function openMenu(){
-        landenOverlay.classList.toggle("visible");
+        landenOverlay.classList.toggle
+        ("visible");
     }
